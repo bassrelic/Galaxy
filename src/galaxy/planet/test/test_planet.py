60dee3acc6340e07396c9bbfba4ae2cc64fa7720
Sebastian <h_p_r@web.de>
2022-03-17 23:30:27 +0100

    Add initial astroid tests

--- src/galaxy/planet/test/test_planet.py
+++ src/galaxy/planet/test/test_planet.py
@@ -1,5 +1,10 @@
 """ Tests for the Planet Module """
+import pygame
+import config
 from galaxy.planet.planet import Planet
+from galaxy.planet.astroid import Astroid
+
+config.screen = pygame.display.set_mode((500, 500))
 
 def test_baseclass_not_instanciable():
     """ Planet is a baseclass and should never be instantiated itself. Path (Image of graphic) must be supplied. """
@@ -10,3 +15,8 @@ def test_baseclass_not_instanciable():
         assert False
     except AttributeError:
         assert True
+
+def test_astroid_get_naem():
+    """ Test instantiation and naming of astroid. """
+    astroid = Astroid("99942 Apophis", 0, 1)
+    assert astroid.get_name() == "99942 Apophis"
