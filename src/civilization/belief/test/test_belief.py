216bd2852efeb1b2c2f313afc17dfe9026167e46
Sebastian <h_p_r@web.de>
2022-03-17 22:48:19 +0100

    Add initial tests to belief system, add get intensity

--- src/civilization/belief/test/test_belief.py
+++ src/civilization/belief/test/test_belief.py
@@ -6,3 +6,11 @@ def test_get_name():
     belief_name = "Pastafarianism"
     belief = Belief(belief_name, 0.5)
     assert belief.get_name() == belief_name
+    assert belief.get_intensity() == 0.5
+
+def test_gen_belief():
+    """ Testing the generate random functionality """
+    belief = Belief.gen_random_belief()
+    assert belief.get_name() == "standard"
+    assert belief.get_intensity() > 0
+    assert belief.get_intensity() < 1
