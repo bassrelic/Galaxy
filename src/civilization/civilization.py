1d77b1cc2866b75ae116f712403db77d25928629
Sebastian <h_p_r@web.de>
2022-04-03 16:56:33 +0200

    add temprange and tests to civilizations

--- src/civilization/civilization.py
+++ src/civilization/civilization.py
@@ -1,5 +1,5 @@
 """This module defined a civilization"""
-from random import random
+from random import randint, random
 from civilization.language.language import Language
 from civilization.belief.belief import Belief
 from technology.technology import Technology
@@ -15,6 +15,11 @@ class Civilization:
         self.technologie_list.append(Technology("Fire"))
         self.belief_list.append(Belief("God", 0.5))
         self.belief_list.append(Belief.gen_random_belief())
+        self.min_atmosphere = random()
+        self.min_temp = randint(-100, 100)
+        self.max_temp = -1000
+        while self.max_temp < self.min_temp:
+            self.max_temp = randint(-100, 100)
 
     def get_name(self):
         """This method returns the name of this civilization"""
@@ -31,6 +36,21 @@ class Civilization:
     def get_count(self):
         """This Method returns the count of individuals this civilization consists of"""
         return self.count
+
+    def get_min_atmosphere(self):
+        """This Method returns the minimal atmosphere value a planet must provide in order to be hospitable for this
+        civilization"""
+        return self.min_atmosphere
+
+    def get_min_temp(self):
+        """This Method returns the minimal temperature value a planet must provide in order to be hospitable for this
+        civilization"""
+        return self.min_temp
+
+    def get_max_temp(self):
+        """This Method returns the maximal temperature value a planet must provide in order to be hospitable for this
+        civilization"""
+        return self.max_temp
 
     def step(self):
         """This method defines the behavour of this civilization per step"""
