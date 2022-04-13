39139b39c5aca80ffa6390f2028e8873cfbbcdca
Sebastian <h_p_r@web.de>
2022-04-13 23:44:38 +0200

    Add technologies, add technology list getter

--- src/technology/technology.py
+++ src/technology/technology.py
@@ -1,4 +1,5 @@
 """This method defines technologies"""
+import json
 class Technology:
     """This class defines technologies"""
     def __init__(self, name):
@@ -7,3 +8,10 @@ class Technology:
     def get_name(self):
         """This method returns the name of this technologie"""
         return self.name
+
+    @classmethod
+    def get_all_technology_list(cls):
+        """This method generates a random beief with random intensity"""
+        with open('src\\technology\\technologies.json', encoding="utf-8") as json_file:
+            data = json.load(json_file)
+        return data
